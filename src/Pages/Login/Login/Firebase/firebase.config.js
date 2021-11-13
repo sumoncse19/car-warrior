94a53d166b259f2273594f3ad30f8bdfba2c8c96
sumon6638-sm <mdsumoncse19@gmail.com>
2021-11-13 08:48:39 +0600

    update_firebase_env

--- src/Pages/Login/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -1,19 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDgcfpAZY2SP7enjbt5VWRZC2p0V_WIp4w",
-    authDomain: "bike-warrior-sumon6638.firebaseapp.com",
-    projectId: "bike-warrior-sumon6638",
-    storageBucket: "bike-warrior-sumon6638.appspot.com",
-    messagingSenderId: "206156754389",
-    appId: "1:206156754389:web:6c5e6a99a4380477a2e15c"
-};
-export default firebaseConfig;
-
-
-/*
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-*/
+};
+export default firebaseConfig;
